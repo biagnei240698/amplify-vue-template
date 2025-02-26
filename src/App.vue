87299016128be8646df6b76c1278fde7200e0929
biagnei240698 <biagnei240698@gmail.com>
2025-02-25 19:39:55 -0600

    fixes add fields create event

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
-import Todos from './components/Todos.vue'
+import Todos from './components/Todos.vue';
+import { Fieldset } from '@aws-amplify/ui-react';
+
 </script>
 
 <template>
@@ -10,6 +12,13 @@ import Todos from './components/Todos.vue'
       <template v-slot="{ user, signOut }">
         <h1>Hello {{user?.signInDetails?.loginId}}'s todos</h1>
         <Todos />
+        <Input placeholder="evento"/>
+        <Input placeholder="fecha"/>
+        <Input placeholder="tipoEvento"/>
+        <Input placeholder="lugar"/>
+        <Input placeholder="nombreEvento"/>
+        <Input placeholder="Baggins"/>
+        <Input placeholder="Baggins"/>
         <button @click="signOut">Sign Out</button>
       </template>
     </authenticator>
